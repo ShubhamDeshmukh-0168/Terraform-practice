28b891cc76afa7bff521e537e446048efb592960
shubham <shubhamndeshmukh02@gmail.com>
2026-03-13 12:46:04 +0530

    dfwfwf

--- Day-6-statelocking/main.tf
+++ Day-6-statelocking/main.tf
@@ -4,3 +4,19 @@ resource "aws_vpc" "name" {
         Name = "sd-vpc"
     }
 }
+resource "aws_instance" "name" {
+    ami = "ami-02dfbd4ff395f2a1b"
+    instance_type = "t2.micro"
+    tags = {
+        Name = "sd-instance"
+    }
+  
+}
+
+resource "aws_subnet" "sample" {
+    vpc_id = aws_vpc.name.id
+    cidr_block = "10.0.0.0/24"
+    tags = {
+        Name = "sd-subnet"
+    }
+}
