702eb1b9d8291429a2b9b1abec9dcbacc744446e
shubham <shubhamndeshmukh02@gmail.com>
2026-04-01 15:28:28 +0530

    Day15

---
+++ Day-15-diff-env-modules/env/dev/variables.tf
@@ -0,0 +1,9 @@
+
+variable "vpc_cidr" {}
+variable "public_subnet_cidr" {}
+variable "availability_zone" {}
+variable "instance_type" {}
+variable "env" {}
+variable "ami_id" {}
+variable "region" {}
+#variable "veera" {}
